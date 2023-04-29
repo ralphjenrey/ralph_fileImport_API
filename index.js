8974b3e85d4c8d66d509b44ed56e34bbf0feba70
ralphjenrey <130786512+ralphjenrey@users.noreply.github.com>
2023-04-29 21:47:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 var express = require('express');
+var multer = require('multer');
 var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Set up Multer middleware
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'upload/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+const upload = multer({ storage: storage })
 
+// Set up route to handle file uploads
+app.post('/upload', upload.single('upfile'), function (req, res, next) {
+  // Get file details
+  const file = req.file;
+  const filename = file.filename;
+  const size = file.size;
 
+  // Return response
+  res.json({
+    message: 'File uploaded successfully',
+    filename: filename,
+    size: size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
